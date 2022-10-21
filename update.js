73c33b27c7e6d3c54c19d93482ab0a8a02d1637f
Viento <107330816+vientorepublic@users.noreply.github.com>
2022-10-21 23:52:53 +0900

    final commit

--- update.js
+++ update.js
@@ -4,89 +4,91 @@
 */
 
 const fs = require('fs');
-const dayjs = require('dayjs');
+const dayjs = require("dayjs");
+let time = dayjs().format("YYYY-MM-DD HH:mm:ss");
 
 const DBpath = "/ssd/MIRROR/logs/status.json";
 let status = process.argv[2];
 let type = process.argv[3];
-const max = 4;
-
-const typelist = {
-    "ubuntu": 0,
-    "putty": 1,
-    "vim": 2,
-    "mplayer": 3,
-};
-
-const time = dayjs().format('YYYY MM DD HH:mm:ss A');
 
 fs.readFile(DBpath, 'utf8', (error, jsonFile) => {
     if (error) return console.log(error);
     data = JSON.parse(jsonFile);
 
-    if (type in typelist && status == "success") {
-        if (data["pkgs"][typelist[type]]["status"] == "syncing") {
-            data["syncing"]--;
+    try {
+        if (type && status == "success") {
+
+            dataset = data["package"][type]["status"];
+
+            if (dataset["updated"] == null) {
+
+                let isoTime = new Date().toISOString();
+
+                let newData = { "timestamp": isoTime };
+
+                let finalData = Object.assign({}, newData);
+                dataset["updated"] = finalData;
+
+            }
+            if (dataset["updating"] != null) {
+
+                dataset["updating"] = null;
+
+            }
+            if (dataset["failed"] != null) {
+
+                dataset["failed"] = null;
+
+            }
+
+            let isoTime = new Date().toISOString();
+            data["package"][type]["status"]["updated"]["timestamp"] = isoTime;
+            console.log(type, `: sync success at ${time}`);
+
+        } else if (type && status == "syncing") {
+
+            let isoTime = new Date().toISOString();
+
+            let newData = { "timestamp": isoTime };
+            let finalData = Object.assign({}, newData);
+
+            data["package"][type]["status"]["updating"] = finalData;
+            console.log(type, `: sync begins at ${time}`);
+
+        } else if (type && status == "failed") {
+
+            dataset = data["package"][type]["status"];
+            let isoTime = new Date().toISOString();
+
+            if (dataset["updating"] != null) {
+
+                dataset["updating"] = null;
+
+            }
+            if (dataset["failed"] != null) {
+
+                dataset["failed"]["timestamp"] = isoTime;
+                dataset["failed"]["count"]++;
+
+            } else {
+
+
+                let newData = { "timestamp": isoTime, "count": Number(1) };
+                let finalData = Object.assign({}, newData);
+
+                dataset["failed"] = finalData;
+
+            }
+
+            console.log(type, `: sync failed at ${time}`);
+
         }
-        if (data["failed_history"][typelist[type]]["isFailed"] == true) {
-            data["failed_history"][typelist[type]]["isFailed"] = false;
-            data["failed"]--;
-        }
-        if (max < data["active"]) {
-            console.log("Exception: active pkgs > max active pkgs");
-            process.exit();
-        }
-        data["pkgs"][typelist[type]]["status"] = "success";
-        data["active"]++;
-        data["pkgs"][typelist[type]]["last_success"] = time;
         fs.writeFile(DBpath, JSON.stringify(data), (err) => {
             if (err) {
                 console.log(err);
             }
         });
+    } catch (e) {
+        console.log(e);
     }
-
-    if (type in typelist && status == "syncing") {
-        if (max < data["syncing"]) {
-            console.log("Exception: active pkgs > max syncing pkgs");
-            process.exit();
-        }
-        if (data["failed_history"][typelist[type]]["isFailed"] == true) {
-            data["failed_history"][typelist[type]]["isFailed"] = false;
-            data["failed"]--;
-            data["active"]++;
-        }
-        data["pkgs"][typelist[type]]["status"] = "syncing";
-        data["syncing"]++;
-        data["active"]--;
-        fs.writeFile(DBpath, JSON.stringify(data), (err) => {
-            if (err) {
-                console.log(err);
-            }
-        });
-    }
-
-    if (type in typelist && status == "failed") {
-        if (data["pkgs"][typelist[type]]["status"] == "syncing") {
-            data["syncing"]--;
-        }
-        if (max < data["failed"]) {
-            console.log("Exception: active pkgs > max failed pkgs");
-            process.exit();
-        }
-        if (max < data["failed"]) {
-            console.log("Exception: active pkgs > max failed pkgs");
-            process.exit();
-        } else {
-            data["failed_history"][typelist[type]]["isFailed"] = true;
-        }
-        data["pkgs"][typelist[type]]["status"] = "failed";
-        data["failed"]++;
-        fs.writeFile(DBpath, JSON.stringify(data), (err) => {
-            if (err) {
-                console.log(err);
-            }
-        });
-    }
-
 });
